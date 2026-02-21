6dd1a71ed546a52e42c2f5c7b2461d75585f5497
DinoZ-p <tomzhaozhenghang@gmail.com>
2026-02-20 21:32:03 -0500

    2/20/2026|Candy|Hard

---
+++ leetcode/candy.py
@@ -0,0 +1,62 @@
+"""
+There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
+
+You are giving candies to these children subjected to the following requirements:
+
+Each child must have at least one candy.
+Children with a higher rating get more candies than their neighbors.
+Return the minimum number of candies you need to have to distribute the candies to the children.
+
+ 
+
+Example 1:
+
+Input: ratings = [1,0,2]
+Output: 5
+Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively.
+Example 2:
+
+Input: ratings = [1,2,2]
+Output: 4
+Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
+The third child gets 1 candy because it satisfies the above two conditions.
+ 
+
+Constraints:
+
+n == ratings.length
+1 <= n <= 2 * 104
+0 <= ratings[i] <= 2 * 104
+"""
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        n = len(ratings)
+        candies = [1] * n
+
+        for i in range(1, n):
+            if ratings[i] > ratings[i - 1]:
+                candies[i] = candies[i - 1] + 1
+
+        for i in range(n - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i], candies[i + 1] + 1)
+
+        return sum(candies)
+
+"""
+the initial idea should be easy, since each child must have at least one candy, 
+we can start with 1 candy for each child. Then we can do two passes, 
+
+one from left to right and one from right to left. In the first pass, 
+we check if the current child has a higher rating than the previous child, 
+if so, we give the current child one more candy than the previous child. 
+
+In the second pass, we check if the current child has a higher rating than the next child, 
+if so, we give the current child one more candy than the next child. 
+
+Finally, we sum up all the candies and return the result.
+"""
