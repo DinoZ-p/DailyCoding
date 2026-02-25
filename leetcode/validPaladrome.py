a1dbbc101f7c0d001166793fc260010e23054649
DinoZ-p <tomzhaozhenghang@gmail.com>
2026-02-25 16:46:49 -0500

    2/25|valid_paladrome|easy

---
+++ leetcode/validPaladrome.py
@@ -0,0 +1,61 @@
+"""
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+"""
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        left = 0
+        right = len(s)-1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+        return True
+    
+"""
+instead of creating a new string with only alphanumeric characters,
+you can use two pointers to compare characters from the original string.
+initialize one pointer at the beginning of the string and another pointer at the end of the string. 
+then you can move the pointers towards each other, skipping any non-alphanumeric characters, 
+and comparing the characters at the pointers.
+
+if you find a mismatch, you can return false. 
+if the pointers cross each other, you can return true.
+"""
