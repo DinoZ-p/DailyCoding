02aecc81649bbcaac668be976e7f82ccaf496360
DinoZ-p <tomzhaozhenghang@gmail.com>
2026-03-03 16:30:54 -0500

    3/3/2026|remove_linkedlist_elements|Easy

---
+++ leetcode/removeLinkedlistElements.py
@@ -0,0 +1,57 @@
+"""
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 104].
+1 <= Node.val <= 50
+0 <= val <= 50
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: Optional[ListNode]
+        :type val: int
+        :rtype: Optional[ListNode]
+        """
+        pointer = ListNode(0)
+        pointer.next = head
+        
+        prev = pointer
+        curr = head
+        while curr:
+            if curr == val:
+                prev.next = curr.next
+            else:
+                prev = curr
+            curr = prev
+        return pointer.next
+"""
+in linked list you cannot delete item directly, instead you skip that item and link to the next item
+
+pointer = ListNode(0) handles the if head == val so we do not skip check head.
+
+"""
