4aaf0e4f50980c4ed473cddc8460b16ee023e3b8
DinoZ-p <tomzhaozhenghang@gmail.com>
2026-03-16 13:13:31 -0400

    group_anagrams|Medium

---
+++ leetcode/groupAnagrams.py
@@ -0,0 +1,55 @@
+"""
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+Example 2:
+
+Input: strs = [""]
+
+Output: [[""]]
+
+Example 3:
+
+Input: strs = ["a"]
+
+Output: [["a"]]
+
+ 
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+"""
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        groups = {}
+    
+        for word in strs:
+            key = "".join(sorted(word))
+            if key not in groups:
+                groups[key] = []
+            groups[key].append(word)
+    
+        return list(groups.values())
+        
+"""
+the trick here is using sort. becuase if the words can be arranged to same words, that means after sorting, they are exactly the same
+so we use hashmap for it. if the sorted version is in the hashmap, we add it to the key , 
+if not, we creat new key and store it as a seperate array.
+"""
+
